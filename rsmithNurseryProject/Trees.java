7393bb0f7e65721607839830aca1039d5a48938c
RyCSmith <rycsmith@gmail.com>
2014-10-21 01:20:10 -0400

    extends Plants. Provides blueprint for Tree plant objects

---
+++ rsmithNurseryProject/Trees.java
@@ -0,0 +1,32 @@
+
+package rsmithNurseryProject;
+
+
+public class Trees extends Plants  {
+	
+	
+	private boolean evergreen;
+	
+	
+	public Trees()
+	{
+		super();
+	}
+	
+	public Trees(double c, String i, int nIS, double p, boolean ever)
+	{
+		super(c, i, nIS, p);
+		evergreen = ever;
+	}
+
+	
+	public boolean isEvergreen() {
+		return evergreen;
+	}
+
+	
+	public void setEvergreen(boolean evergreen) {
+		this.evergreen = evergreen;
+	}
+
+}

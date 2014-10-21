2242a5a8ae0c03a4f0f0db58da73e786fa2fb0b2
RyCSmith <rycsmith@gmail.com>
2014-10-21 01:18:18 -0400

    abstract class that extends Inventory. Lays blueprint for Plant objects

---
+++ rsmithNurseryProject/Plants.java
@@ -0,0 +1,15 @@
+
+package rsmithNurseryProject;
+
+public abstract class Plants extends Inventory {
+
+	public Plants()
+	{
+		super();
+	}
+	
+	public Plants(double c, String i, int nIS, double p)
+	{
+		super(c, i, nIS, p);
+	}
+}
